2ac15afa9761b4c4ea196a26ce861bbea3a7b6a7
Israel Canessa <israellcanessa@gmail.com>
2020-07-09 02:11:43 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,13 +9,11 @@ end
 
 def long_planeteer_calls(planeteer_calls)
   planeteer_calls.any? { |element| element.size > 4 }
-  # Use an Enumerable to check if any string in the passed in array is greater than 4 characters long
-  # Return the boolean result of this check
+
 end
 
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
   planeteer_calls.find {|element| valid_calls.include?("#{element}")}
-  # Use an Enumerable to check if any elements in the passed in array match the valid calls listed above
-  # Return the first valid call found, or return nil if no valid calls are found
+
 end
